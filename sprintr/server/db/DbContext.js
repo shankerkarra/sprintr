b546626bccbb3ee0c0619ba178cd6165399bf974
Jonathan Wright <j.wright72127@gmail.com>
2021-08-09 14:01:04 -0600

    Cleaned Up the Server

--- sprintr/server/db/DbContext.js
+++ sprintr/server/db/DbContext.js
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose'
-import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 }
 

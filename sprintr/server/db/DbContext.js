7bc525c61529d22ece7259b7e1e9d1e46615ec3b
Jonathan Wright <j.wright72127@gmail.com>
2021-08-09 14:30:06 -0600

    Edited DbContext

--- sprintr/server/db/DbContext.js
+++ sprintr/server/db/DbContext.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
-import { ProjectSchema } from '../models/Project'
-import { BacklogSchema } from '../models/Backlog'
-import { SprintSchema } from '../models/Sprint'
-import { TaskSchema } from '../models/Task'
-import { NoteSchema } from '../models/Note'
+import { Project } from '../models/Project'
+import { BackLogItem } from '../models/BackLogItem'
+import { Sprint } from '../models/Sprint'
+import { Task } from '../models/Task'
+import { Note } from '../models/Note'
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
-  Project = mongoose.model('Project', ProjectSchema);
-  Backlog = mongoose.model('Backlog', BacklogSchema);
-  Sprint = mongoose.model('Sprint', SprintSchema);
-  Task = mongoose.model('Task', TaskSchema);
-  Note = mongoose.model('Note', NoteSchema);
+  Project = mongoose.model('Project', Project);
+  Backlog = mongoose.model('Backlog', BackLogItem);
+  Sprint = mongoose.model('Sprint', Sprint);
+  Task = mongoose.model('Task', Task);
+  Note = mongoose.model('Note', Note);
 }
 
 export const dbContext = new DbContext()

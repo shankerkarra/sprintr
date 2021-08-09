0f6969003d98ecfe480dd8fd6e7b6c86e8d95e30
Shanker Karra <shankerkarra@gmail.com>
2021-08-09 16:28:28 -0600

    check in

--- sprintr/server/models/Project.js
+++ sprintr/server/models/Project.js
@@ -2,13 +2,12 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
-
 export const Project = new Schema(
   {
     // id: {type: ObjectId, required: true},
-    name : {type: String, unique: true},
-    description: {type: String, required: true},
-    creatorId: {type: ObjectId, ref: 'Account', required: true}
+    name: { type: String, unique: true },
+    description: { type: String, required: true },
+    creatorId: { type: ObjectId, ref: 'Account', required: true }
   }
 )
 Project.virtual('account', {

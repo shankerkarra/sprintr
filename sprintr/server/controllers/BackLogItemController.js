c9145d8d03133eea6e6bada4e20957fefb40e657
Jonathan Wright <j.wright72127@gmail.com>
2021-08-09 15:34:11 -0600

    Finshed part 1 backlog component

--- sprintr/server/controllers/BackLogItemController.js
+++ sprintr/server/controllers/BackLogItemController.js
@@ -1,12 +1,47 @@
+import { backlogService } from "../services/BacklogService"
+import { taskService } from "../services/TaskService"
 import BaseController from "../utils/BaseController"
 
 export class BackLogItemController extends BaseController{
   constructor() {
     super('api/backlog')
     this.router
-      .get('')
-      .get('/:id')
-      .get('/:id/tasks')
-      .post
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .get('/:id/tasks', this.getAllTasksByBacklog)
+      .post('', this.create)
+  }
+  async getAll(req, res, next) {
+    try {
+      const backlogs = await backlogService.getAll(req.query)
+      res.send(backlogs)
+    } catch (error) {
+      next('We had a problem getting the Backlog Items : ', error)
+    }
+  }
+  async getById(req, res, next) {
+    try {
+      const backlog = await backlogService.getById(req.params.id)
+      res.send(backlog)
+    } catch (error) {
+      next('We had a problem getting that Backlog Item : ', error)
+    }
+  }
+  async getAllTasksByBacklog(req, res, next) {
+    try {
+      const task = await taskService.getAll({ backlogId: req.params.id })
+      res.send(task)
+    } catch (error) {
+      next('We had a problem getting those Tasks for this Backlog Item : ', error)
+    }
+  }
+  async create(req, res, next) {
+    try {
+      const user = req.userInfo
+      req.body.creatorId = user.id
+      const backlog = await backlogService.createBacklog(req.body)
+    } catch (error) {
+      next('We had a problem creating that Backlog Item : ', error)
+    }
   }
 }

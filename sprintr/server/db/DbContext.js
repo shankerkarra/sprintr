287b2d643182e42ca52a3aac9532641dcb761644
Shanker Karra <shankerkarra@gmail.com>
2021-08-09 14:22:01 -0600

    Checkin dbcontext and ps

--- sprintr/server/db/DbContext.js
+++ sprintr/server/db/DbContext.js
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
-
+import { ProjectSchema } from '../models/Project'
+import { BacklogSchema } from '../models/Backlog'
+import { SprintSchema } from '../models/Sprint'
+import { TaskSchema } from '../models/Task'
+import { NoteSchema } from '../models/Note'
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Project = mongoose.model('Project', ProjectSchema);
+  Backlog = mongoose.model('Backlog', BacklogSchema);
+  Sprint = mongoose.model('Sprint', SprintSchema);
+  Task = mongoose.model('Task', TaskSchema);
+  Note = mongoose.model('Note', NoteSchema);
 }
 
 export const dbContext = new DbContext()
--- sprintr/server/services/ProjectService.js
+++ sprintr/server/services/ProjectService.js
@@ -2,7 +2,17 @@ import { BadRequest } from '../utils/Errors'
 import { dbContext } from '../db/DbContext'
 
 class ProjectService {
+  async getAll(query = {}) {
+    return await dbContext.Project.find(query)
+  }
 
+  async getById(id) {
+    const project = await dbContext.Project.findById(id)
+    if (!project) {
+      throw new BadRequest('Invalid Project ID')
+    }
+    return project
+  }
 }
 
 export const projectService = new ProjectService()

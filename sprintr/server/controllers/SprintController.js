8b5a793038a0712f8c841c17b1b236940f2cbd80
Jonathan Wright <j.wright72127@gmail.com>
2021-08-09 15:47:54 -0600

    Added Part 1 To Sprint Controller

---
+++ sprintr/server/controllers/SprintController.js
@@ -0,0 +1,48 @@
+import { sprintService } from "../services/SprintService";
+import { taskService } from "../services/TaskService";
+import BaseController from "../utils/BaseController";
+
+export class SprintController extends BaseController{
+  constructor() {
+    super('api/sprint')
+    this.router
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .get('/:id/tasks', this.getAllTasksBySprint)
+      .post('', this.create)
+  }
+  async getAll(req, res, next) {
+    try {
+      const sprints = await sprintService.getAll(req.query)
+      res.send(sprints)
+    } catch (error) {
+      next('We had a problem getting the Sprints : ', error)
+    }
+  }
+  async getById(req, res, next) {
+    try {
+      const sprint = await sprintService.getById(req.params.id)
+      res.send(sprint)
+    } catch (error) {
+      next('We had a problem getting that Sprint : ', error)
+    }
+  }
+  async getAllTasksBySprint(req, res, next) {
+    try {
+      const task = await taskService.getAll({ sprintId: req.params.id })
+      res.send(task)
+    } catch (error) {
+      next('We had a problem getting the Tasks for that Sprint', error)
+    }
+  }
+  async create(req, res, next) {
+    try {
+      const user = req.userInfo
+      req.body.creatorId = user.id
+      const sprint = await sprintService.createSprint(req.body)
+      res.send(sprint)
+    } catch (error) {
+      next('We had a problem creating that Sprint', error)
+    }
+  }
+}

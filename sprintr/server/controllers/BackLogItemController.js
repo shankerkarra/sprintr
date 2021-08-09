c32b07e6665577695c2a189af529351694d6944c
Jonathan Wright <j.wright72127@gmail.com>
2021-08-09 15:23:22 -0600

    Added .post

--- sprintr/server/controllers/BackLogItemController.js
+++ sprintr/server/controllers/BackLogItemController.js
@@ -1,5 +1,12 @@
 import BaseController from "../utils/BaseController"
 
-export class BackLogItemController extend BaseController{
-  
+export class BackLogItemController extends BaseController{
+  constructor() {
+    super('api/backlog')
+    this.router
+      .get('')
+      .get('/:id')
+      .get('/:id/tasks')
+      .post
+  }
 }

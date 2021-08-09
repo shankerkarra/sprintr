a22d9cb8d322e7d9fb8a7465191328193ed5d3e2
Shanker Karra <shankerkarra@gmail.com>
2021-08-09 14:56:02 -0600

    check in

--- sprintr/server/services/NoteService.js
+++ sprintr/server/services/NoteService.js
@@ -2,7 +2,33 @@ import { BadRequest } from '../utils/Errors'
 import { dbContext } from '../db/DbContext'
 
 class NoteService {
+  async getAll(query = {}) {
+    return await dbContext.Note.find(query)
+  }
 
+  async getById(id) {
+    const note = await dbContext.Note.findById(id)
+    if (!note) {
+      throw new BadRequest('Invalid ID')
+    }
+    return note
+  }
+
+  async getByTaskId(taskid) {
+    const note = await dbContext.Note.findByTaskId(taskid)
+    if (!note) {
+      throw new BadRequest('Invalid Task ID')
+    }
+    return note
+  }
+
+  async getByCreatorId(taskid) {
+    const note = await dbContext.Note.findByCreatorId(creatorid)
+    if (!note) {
+      throw new BadRequest('Invalid Creator ID')
+    }
+    return note
+  }
 }
 
 export const noteService = new NoteService()
--- sprintr/server/services/TaskService.js
+++ sprintr/server/services/TaskService.js
@@ -2,7 +2,49 @@ import { BadRequest } from '../utils/Errors'
 import { dbContext } from '../db/DbContext'
 
 class TaskService {
+  async getAll(query = {}) {
+    return await dbContext.Task.find(query)
+  }
 
+  async getById(id) {
+    const task = await dbContext.Task.findById(id)
+    if (!task) {
+      throw new BadRequest('Invalid ID')
+    }
+    return task
+  }
+
+  async getByProjectId(projectid) {
+    const task = await dbContext.Task.findByProjectId(projectid)
+    if (!task) {
+      throw new BadRequest('Invalid Project ID')
+    }
+    return task
+  }
+
+  async getBySprintId(sprintid) {
+    const task = await dbContext.Task.findBySprintId(sprintid)
+    if (!task) {
+      throw new BadRequest('Invalid Sprint ID')
+    }
+    return task
+  }
+
+  async getByBacklogId(backlogid) {
+    const task = await dbContext.Task.findByBacklogId(backlogid)
+    if (!task) {
+      throw new BadRequest('Invalid Project ID')
+    }
+    return task
+  }
+
+  async getByCreatorId(taskid) {
+    const task = await dbContext.Task.findByCreatorId(creatorid)
+    if (!task) {
+      throw new BadRequest('Invalid Creator ID')
+    }
+    return task
+  }
 }
 
 export const taskService = new TaskService()

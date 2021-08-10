bbf5b40dcbf4d78b3dad2561fcc05f7e6629fffe
Jonathan Wright <j.wright72127@gmail.com>
2021-08-10 10:54:46 -0600

    Fixed Stuff

--- sprintr.client/src/services/ProjectService.js
+++ sprintr.client/src/services/ProjectService.js
@@ -3,33 +3,32 @@ import { logger } from '../utils/Logger'
 import {api} from './AxiosService'
 
 class ProjectService {
-  async getAllProject(query) {
+  async getAll(query = {}) {
     const res = await api.get('api/projects')
     logger.log('All the projects : ', res.data)
     AppState.projects = res.data
   }
 
-  async getProjectById(id) {
+  async getById(id) {
     const res = await api.get('api/projects/' + id)
-    if(!res)
     logger.log('A single project : ', res.data)
     AppState.project = res.data
   }
 
-  async addProject(body) {
+  async create(body) {
     const res = await api.post('api/projects', body)
     logger.log('Added A project : ', res.data)
     AppState.project.push(res.data)
     return res.data.id
   }
 
-  async removeProject(id) {
+  async destroy(id) {
     await api.delete('api/projects/' + id)
     AppState.project = AppState.project.find(p => p.id !== id)
     logger.log('Deleted Successfully')
   }
 
-  async editProject(id, body) {
+  async update(id, body) {
     const res = api.put('api/projects/' + id, body)
     logger.log('Edited A project : ', res.data)
     AppState.project = res.data

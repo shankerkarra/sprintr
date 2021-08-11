edc8ed2904c19cc610416ec7d8fa727299eaf41a
Jonathan Wright <j.wright72127@gmail.com>
2021-08-11 10:27:20 -0600

    Fixed Task Backlog

--- sprintr/server/models/Task.js
+++ sprintr/server/models/Task.js
@@ -7,7 +7,7 @@ export const Task = new Schema(
     status: { type: String, required: true },
     weight: { type: Number, required: true },
     projectId: { type: ObjectId, ref: 'Project', required: true },
-    sprintId: { type: ObjectId, ref: 'Sprint', required: true },
+    sprintId: { type: ObjectId, ref: 'Sprint' },
     backlogId: { type: ObjectId, ref: 'BackLogItem', required: true },
     creatorId: { type: ObjectId, ref: 'Account', required: true }
   }

b4b74e7a9985bd41315fdc678925a232e1ebe22d
Jonathan Wright <j.wright72127@gmail.com>
2021-08-11 12:08:59 -0600

    Made Sprints work

--- sprintr.client/src/pages/ProjectSprintPage.vue
+++ sprintr.client/src/pages/ProjectSprintPage.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="row bg-dark justify-content-start">
+    <div class="col-md-2 col-12 text-light">
+      <h5 class="hoverable" data-toggle="modal" data-target="#exampleModalCenter">
+        {{ project.name }}
+      </h5>
+
+      <div class="modal fade"
+           id="exampleModalCenter"
+           tabindex="-1"
+           role="dialog"
+           aria-labelledby="exampleModalCenterTitle"
+           aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered text-dark" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">
+                {{ project.name }}
+              </h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" v-if="!project.description">
+              <p>No Description Provided</p>
+            </div>
+            <div class="modal-body" v-else>
+              <p>{{ project.description }}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="col-md-2 col-6 border border-dark text-center bg-grey">
       <router-link :to="{ name: 'ProjectBacklog' }">
         <h4> Backlog </h4>
@@ -12,21 +49,32 @@
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core'
+import { computed, onMounted } from '@vue/runtime-core'
 import { useRoute } from 'vue-router'
 import Pop from '../utils/Notifier'
 import { projectService } from '../services/ProjectService'
+import { AppState } from '../AppState'
 export default {
   name: 'ProjectSprint',
   setup() {
     const route = useRoute()
     onMounted(async() => {
       try {
+        await projectService.getById(route.params.id)
         await projectService.getSprintsByProject(route.params.id)
       } catch (error) {
         Pop.toast('Error fetching Sprints', error)
       }
     })
+    return {
+      project: computed(() => AppState.activeProject)
+    }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.hoverable {
+  cursor: pointer;
+}
+</style>
